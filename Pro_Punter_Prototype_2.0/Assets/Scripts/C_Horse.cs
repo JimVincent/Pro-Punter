715d7c46d77e403bdb6f010b68dac59af1f4b669
JimVincent <1000343@student.sae.edu.au>
2015-01-16 16:15:57 +1000

    Initial Set Up
    Added folders and some of the scripts. Yet to import the Mic scripts.
    
    Text script in and working

---
+++ Pro_Punter_Prototype_2.0/Assets/Scripts/C_Horse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C_Horse : MonoBehaviour 
+{
+	public string mName;
+	public float mSpeed;
+	public float mValue;
+	public bool isPlayer = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
