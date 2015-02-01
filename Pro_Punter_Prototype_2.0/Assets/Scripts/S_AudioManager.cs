fae1e5fb31797b3380c24b6219450fa4c19b08d3
JimVincent <1000343@student.sae.edu.au>
2015-02-02 08:37:30 +1000

    Updated some Menu UI Functions
    Still having some issues with running functions on slider move for
    volumes.

---
+++ Pro_Punter_Prototype_2.0/Assets/Scripts/S_AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_AudioManager : MonoBehaviour {
+
+	static public S_AudioManager inst;
+
+	public enum Type {SFX, Music};
+
+	[System.Serializable]
+	public class ASource
+	{
+		public AudioSource aSource;
+		public Type type;
+		public float original = 1.0f;
+	}
+
+	public ASource[] aSources;
+
+	private float sfxVolume = 1.0f;
+	private float musicVolume = 1.0f;
+
+	void Awake()
+	{
+		inst = this;
+	}
+
+	void Start()
+	{
+		for(int i = 0; i < aSources.Length; i++)
+		{
+			aSources[i].original = aSources[i].aSource.volume;
+		}
+	}
+
+	public void UpdateVolumes()
+	{
+		sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+		musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+
+		for(int i = 0; i < aSources.Length; i++)
+		{
+			float newVolume;
+
+			if(aSources[i].type == Type.Music)
+			{
+				newVolume = aSources[i].original * PlayerPrefs.GetFloat("MusicVolume");
+			}
+			else
+			{
+				newVolume = aSources[i].original * PlayerPrefs.GetFloat("SFXVolume");
+			}
+
+			aSources[i].aSource.volume = newVolume;
+		}
+	}
+
+	public void SaveMusicVolume(int music)
+	{
+		PlayerPrefs.SetFloat("musicVolume", music / 10);
+		UpdateVolumes();
+	}
+
+	public void SaveSFXVolume(int sfx)
+	{
+		PlayerPrefs.SetFloat("SFXVolume", sfx / 10);
+		UpdateVolumes();
+	}
+}
---
+++ Pro_Punter_Prototype_2.0/Assets/Scripts/S_UI_Menu.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class S_UI_Menu : MonoBehaviour {
+
+	public enum State {main, options, exit};
+	public enum Button {start, options, exit, back};
+
+	[System.Serializable]
+	public class UIScreen
+	{
+		public State name;
+		public Canvas canvas;
+	}
+
+	public UIScreen[] screens;
+	public Slider[] sliders;
+
+	// Use this for initialization
+	void Start () 
+	{
+		SwitchScreen(State.main);
+	}
+
+	// switches on active state and switches off rest
+	public void SwitchScreen(State newState)
+	{
+		for(int i = 0; i < screens.Length; i++)
+		{
+			if(newState == screens[i].name)
+			{
+				screens[i].canvas.enabled = true;
+			}
+			else
+			{
+				screens[i].canvas.enabled = false;
+			}
+		}
+	}
+
+	public void ButtonPress(Button button)
+	{
+		switch(button)
+		{
+		// initiate start game sequence
+		case Button.start:
+
+			break;
+		
+		// switch canvas's
+		case Button.options:
+			SwitchScreen(State.options);
+			break;
+
+		// quit the game
+		case Button.exit:
+			Application.Quit();
+			break;
+
+		// save options data and switch to main
+		case Button.back:
+			SwitchScreen(State.main);
+			break;
+		}
+	}
+}
